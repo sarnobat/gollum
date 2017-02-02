49fcd8b171eda9e0daaf84728c8204362615018c
Sridhar Sarnobat <sridhar.sarnobat@itsoninc.com>
2017-02-02 15:26:11 -0800

    Cron daily autocommit

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-bundle exec bin/gollum --page-file-dir /Users/ssarnobat/sarnobat.git/mwk
+echo "Open http://localhost:4567/pages"
+bundle exec bin/gollum /Users/ssarnobat/sarnobat.git/ --h1-title 
+#--page-file-dir /Users/ssarnobat/sarnobat.git/mwk
 
-#open "http://localhost:4567/"
